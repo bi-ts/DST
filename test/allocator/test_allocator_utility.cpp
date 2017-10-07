5e4943d7b2303a2c7b12316b679de08630a541b0
MaksymB <maksym.bilinets+dst@gmail.com>
2017-10-07 16:21:44 +0300

    [test] fix Windows build warning

--- test/allocator/test_allocator_utility.cpp
+++ test/allocator/test_allocator_utility.cpp
@@ -1,5 +1,5 @@
 
-//          Copyright Maksym V. Bilinets 2015 - 2016.
+//          Copyright Maksym V. Bilinets 2015 - 2017.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt )
@@ -75,7 +75,7 @@ BOOST_AUTO_TEST_CASE(test_allocate_aligned)
 
     const auto p_data = dst::memory::allocate_aligned(allocator, 1, i);
 
-    BOOST_TEST(allocator.allocated() >= 1);
+    BOOST_TEST(allocator.allocated() >= 1u);
 
     BOOST_TEST(reinterpret_cast<std::intptr_t>(p_data) % i == 0);
 
