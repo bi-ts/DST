a65d14b4c7f5c210252dd43e1c85919aff45e7fa
MaksymB <maksym.bilinets+dst@gmail.com>
2017-12-17 13:23:01 +0200

    [license] fix copyright notice to reference LICENSE.txt file instead of LICENSE

--- test/allocator/test_global_counter_allocator.cpp
+++ test/allocator/test_global_counter_allocator.cpp
@@ -1,7 +1,7 @@
 
 //          Copyright Maksym V. Bilinets 2015 - 2016.
 // Distributed under the Boost Software License, Version 1.0.
-//        (See accompanying file LICENSE or copy at
+//      (See accompanying file LICENSE.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt )
 
 #include <dst/allocator/global_counter_allocator.h>
